1c5c1707287efbd5466d7e703c3c20775312ae71
wonjinsong <msmk22239@gmail.com>
2021-01-24 21:57:43 -0500

    lotto class2

--- lottoClass.jsx
+++ lottoClass.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 
 class Lotto extends Component {
+    state = {
+        winNumbers : getWinNumbers(),
+        winBalls : [],
+        bonus : null,
+        redo : false,
+    };
     render() {
+        const { winBalls, bonus, redo } = this.state;
         return (
-            <div>
-                
-            </div>
-        )
+            <>
+                <div>Win Numbers</div>
+                <div id="result">
+                    {winBalls.map((v) =><Ball key={v} number={v} />)}
+                </div>
+                <div>Bonus!</div>
+                {bonus && <Ball number={bonus}/>}
+         
+            </>
+        );
     }
 }
 export default Lotto;
